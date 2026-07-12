8dc743f25d1a27316591e2e3136c8cbdab85d932
Nicholas Clark <nicholasliemclark@gmail.com>
2026-07-11 19:55:16 -0700

    Item names

--- wow_ah_dbt/models/marts/fct_item_snapshot.sql
+++ wow_ah_dbt/models/marts/fct_item_snapshot.sql
@@ -12,5 +12,19 @@ aggregated as (
         round(stddev(unit_price_gold)::numeric / nullif(min(unit_price_gold), 0), 4) as price_cv
     from stg
     group by item_id, snapshot_timestamp
+),
+lookup as (
+    select * from {{ ref('item_lookup') }}
 )
-select * from aggregated
+select
+    a.item_id,
+    l.item_name,
+    a.snapshot_timestamp,
+    a.min_price_gold,
+    a.total_quantity,
+    a.listing_count,
+    a.price_stddev,
+    a.price_cv
+from aggregated a
+left join lookup l
+    on a.item_id = l.item_id
